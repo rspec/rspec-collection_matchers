f26f1510468381f8d46a68087826cf3d90b7a3ff
Myron Marston <myron.marston@gmail.com>
2014-01-10 15:59:51 -0800

    Deal with APIs that have been removed from rspec-expectations.
    The expression generator got removed.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,3 +9,44 @@ RSpec.configure do |config|
 
   config.order = 'random'
 end
+
+
+shared_context "with #should enabled", :uses_should do
+  orig_syntax = nil
+
+  before(:all) do
+    orig_syntax = RSpec::Matchers.configuration.syntax
+    RSpec::Matchers.configuration.syntax = [:expect, :should]
+  end
+
+  after(:all) do
+    RSpec::Matchers.configuration.syntax = orig_syntax
+  end
+end
+
+shared_context "with #should exclusively enabled", :uses_only_should do
+  orig_syntax = nil
+
+  before(:all) do
+    orig_syntax = RSpec::Matchers.configuration.syntax
+    RSpec::Matchers.configuration.syntax = :should
+  end
+
+  after(:all) do
+    RSpec::Matchers.configuration.syntax = orig_syntax
+  end
+end
+
+shared_context "with #expect exclusively enabled", :uses_only_expect do
+  orig_syntax = nil
+
+  before(:all) do
+    orig_syntax = RSpec::Matchers.configuration.syntax
+    RSpec::Matchers.configuration.syntax = :expect
+  end
+
+  after(:all) do
+    RSpec::Matchers.configuration.syntax = orig_syntax
+  end
+end
+
