107c5d57e4fb0926c04170c70eafabe24e4eb91d
Bradley Schaefer <bradley.schaefer@gmail.com>
2014-02-20 14:19:30 -0600

    Cleanup based on PR feedback

--- lib/rspec/collection_matchers.rb
+++ lib/rspec/collection_matchers.rb
@@ -1,4 +1,4 @@
 require 'rspec/collection_matchers/version'
 require 'rspec/collection_matchers/matchers'
 require 'rspec/collection_matchers/have'
-require 'rspec/collection_matchers/rails' if defined? Rails
+require 'rspec/collection_matchers/rails' if defined? Rails && respond_to?(:alias_method_chain)
